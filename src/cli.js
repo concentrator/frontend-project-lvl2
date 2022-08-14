e4b63063b97faac43eecf4c3f55f9727ea7e80a3
Dmitry Iovev <iovlev.d@gmail.com>
2022-08-14 21:41:48 +0300

    Adds format option

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,18 @@
 import { Command } from 'commander';
+
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .usage('[options] <filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .configureHelp({
+    // sortOptions: true,
+  });
 
 export default () => {
   program.parse();
-}
+};
