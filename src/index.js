7790f259894488f130807d78e250c61b4bd42f99
Dmitry Iovev <iovlev.d@gmail.com>
2022-09-13 20:36:10 +0300

    Refactoring 6

--- src/index.js
+++ src/index.js
@@ -7,35 +7,34 @@ import formatDiff from './formatters/index.js';
 const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
 
-const buildDiff = (dataFirst, dataSecond) => {
-  const iter = (data1, data2) => {
-    const compareProps = (key) => {
-      const firstValue = data1[key];
-      const secondValue = data2[key];
-      if (_.isObject(firstValue) && _.isObject(secondValue)) {
-        return { name: key, type: 'parent', children: iter(firstValue, secondValue) };
-      }
-      if (!_.has(data1, key)) {
-        return { name: key, type: 'added', value: secondValue };
-      }
-      if (!_.has(data2, key)) {
-        return { name: key, type: 'deleted', value: firstValue };
-      }
-      if (firstValue !== secondValue) {
-        return {
-          name: key,
-          type: 'changed',
-          firstValue,
-          secondValue,
-        };
-      }
-      return { name: key, type: 'unchanged', value: firstValue };
-    };
-    const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
-    return keys.map(compareProps);
+const iterate = (data1, data2) => {
+  const compareProps = (key) => {
+    const firstValue = data1[key];
+    const secondValue = data2[key];
+    if (_.isObject(firstValue) && _.isObject(secondValue)) {
+      return { name: key, type: 'parent', children: iterate(firstValue, secondValue) };
+    }
+    if (!_.has(data1, key)) {
+      return { name: key, type: 'added', value: secondValue };
+    }
+    if (!_.has(data2, key)) {
+      return { name: key, type: 'deleted', value: firstValue };
+    }
+    if (firstValue !== secondValue) {
+      return {
+        name: key,
+        type: 'changed',
+        firstValue,
+        secondValue,
+      };
+    }
+    return { name: key, type: 'unchanged', value: firstValue };
   };
-  return { type: 'root', children: iter(dataFirst, dataSecond) };
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+  return keys.map(compareProps);
 };
+
+const buildDiff = (data1, data2) => ({ type: 'root', children: iterate(data1, data2) });
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const parsedFileData1 = parse(readFileSync(buildFullPath(filepath1)), extractFormat(filepath1));
